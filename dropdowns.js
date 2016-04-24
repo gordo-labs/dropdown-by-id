083b9a3a4c22d1648aa728d5de74934aa6c8b9a8
Sergio <sergormo@gmail.com>
2016-04-24 02:10:02 +0200

    Update dropdowns.js
    corrected version, for web and mobile menus

--- dropdowns.js
+++ dropdowns.js
@@ -1,33 +1,24 @@
-
 function dropDown(idLauncher, idDropdown) {
   var dropDown = document.getElementById(idDropdown);
   var launcher = document.getElementById(idLauncher);
   window.addEventListener('click', function (event) {
-    if (event.target.id == idLauncher) {
+    var tar = event.target.id;
+    if ((tar === idLauncher) && (dropDown.style.display === "block")) {
+      dropDown.style.display = "none";
+      console.log('apagado-laucher');
+    }
+    else if ((dropDown.style.display === "none") && (tar === idDropdown || tar === idLauncher)) {
       dropDown.style.display = "block";
     }
     else {
       dropDown.style.display = "none";
     }
   });
-}
-
-function dropDownTouch(idLauncher, idDropdown) {
-  var dropDown = document.getElementById(idDropdown);
-  var launcher = document.getElementById(idLauncher);
-  launcher.onclick = function () {
-    if (dropDown.style.display === "none") {
-      dropDown.style.display = "block";
-    } else {
-      dropDown.style.display = "none";
-    }
-  }
-  window.onscroll= function () {
-      dropDown.style.display = "none";
+  window.onscroll = function () {
+    dropDown.style.display = "none";
   }
 }
 
 window.onload = function() {
   dropDown('launcher', 'dropdown');
-  dropDownTouch('launcher', 'dropdown');
 };
